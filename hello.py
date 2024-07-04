275655fd551ce7cc469017b22417c079991548ae
codesantaisai <124557050+codesantaisai@users.noreply.github.com>
2024-07-04 09:42:39 +0530

    operators

---
+++ hello.py
@@ -0,0 +1,14 @@
+hi="Hello world!"
+print(hi)
+
+
+line1 = "************"
+line2 = "            "
+line3 = "Jathurshan Santhirasekarm"
+
+
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
---
+++ operators.py
@@ -0,0 +1,6 @@
+a=9
+
+if a>4:
+    print("a is greater than 4")
+else:
+    print("a is not greater than 4")
